32d911f466e19a38cc8f3451962ffe04369f99aa
Emirhan Demirbaşa <emirhanlife@gmail.com>
2025-10-12 16:48:30 +0300

    Header parçası oluşturuldu.

---
+++ public/partials/header.php
@@ -0,0 +1,15 @@
+<?php $baslik = "Let's Quiz | Hoşgeldiniz!"?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $baslik ?? "";?></title>
+    <link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+
+/>
+</head>
+<body>
